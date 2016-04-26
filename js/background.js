6d429ee44a1c514180faa83f0d133814b67747d5
Tim Andrews <anxdpanic@users.noreply.github.com>
2016-04-26 03:37:01 -0400

    [trakt2kodiaddon] background settings
    - refactor settings

--- js/background.js
+++ js/background.js
@@ -1,88 +1,32 @@
-var settings = null;
-
-
-load_settings();
-
-
-chrome.runtime.onConnect.addListener(function (port) {
-    console.assert(port.name == 'T2KASocket');
-    port.onMessage.addListener(function (msg) {
-        switch (msg.action) {
-            case 'save_settings':
-                if (msg.settings) {
-                    save_settings(msg.settings);
-                }
-                else {
-                  console.log('T2KASocket |save_settings| missing |settings|');
-                }                
-                break;        
-            case 'execute_addon':
-                if (msg.params) {
-                    load_settings(function () {
-                        execute_rpc(msg.action, msg.params)
-                    });
-                }
-                else {
-                  console.log('T2KASocket |execute_addon| missing |params|');
-                }
-                break;
-            case 'with_settings':
-                if (msg.cb_functions) {
-                    load_settings(function() {
-                        port.postMessage({ 
-                          action: 'with_settings', 
-                          settings: settings,
-                          cb_functions: msg.cb_functions
-                        });                        
-                    });
-                }
-                else {
-                  console.log('T2KASocket |with_settings| missing |cb_functions|');
-                }
-                break;            
-            default:
-                console.log('T2KASocket |No valid action provided|');
-        }
-    });
-});
-
-
-chrome.webNavigation.onHistoryStateUpdated.addListener(function (details) {
-    if (details.url.indexOf('://trakt.tv/') > -1) {
-        if ((settings.input_ip) && (settings.input_port) && (settings.input_addonid)) {
-            chrome.tabs.executeScript(details.tabId, {
-              file: '/js/content_script.js'
-            });
-            chrome.tabs.insertCSS(details.tabId, {
-              file: '/css/trakt.css'
-            });
-        }
-    }
-});
-
-
-function save_settings(new_settings) {
-    if (chrome.storage.sync) {
-        chrome.storage.sync.set(
-            new_settings, 
-            function () {
-                settings = new_settings;
+var settings = {
+    _storage: function (action, arg1, arg2) {
+        var has_sync = (chrome.storage.sync !== undefined) ? true : false;
+        if (has_sync) {
+            switch (action) {
+                case 'set':
+                    chrome.storage.sync.set(arg1, arg2);
+                    break;
+                case 'get':
+                    chrome.storage.sync.get(arg1, arg2);
+                    break;
+                default:
+                    break;
             }
-        );    
-    }
-    else {
-        chrome.storage.local.set(
-            new_settings, 
-            function () {
-                settings = new_settings;
-            }
-        );          
-    }    
-}
-
-
-function load_settings (callback) {
-    var default_settings = {
+        }
+        else {
+            switch (action) {
+                case 'set':
+                    chrome.storage.local.set(arg1, arg2);
+                    break;
+                case 'get':
+                    chrome.storage.local.get(arg1, arg2);
+                    break;
+                default:
+                    break;
+            } 
+        }
+    },
+    defaults: {
             input_ip: '',
             input_port: '9090',
             input_addonid: '',
@@ -90,33 +34,34 @@ function load_settings (callback) {
             input_episode_show_play: false,
             input_episode_open_season: false,
             input_output_format: '1'      
-    }   
-    if (chrome.storage.sync) {
-        chrome.storage.sync.get(
-            default_settings, 
+    },
+    get: (this.defaults),
+    save: function (new_settings) {
+        settings._storage(
+            'set',
+            new_settings, 
+            function () {
+                settings.get = new_settings;
+            }
+        );
+    },
+    load: function (callback) {
+        settings._storage(
+            'get',
+            settings.defaults, 
             function (items) {
-                settings = items;
+                settings.get = items;
                 if (callback) {
                     callback();
                 }
         });
     }
-    else {
-        chrome.storage.local.get(
-            default_settings, 
-            function (items) {
-                settings = items;
-                if (callback) {
-                    callback();
-                }
-        });            
-    }
-}
+};
 
 
 function execute_rpc(action, params) {
-    var ip = settings.input_ip;  
-    var port = settings.input_port;
+    var ip = settings.get.input_ip;  
+    var port = settings.get.input_port;
     var ws_url = 'ws://' + ip + ':' + port + '/jsonrpc';
     var log_lead = 'execute_rpc\r\n|url| ' + ws_url + '\r\n';
     switch (action) {
@@ -150,9 +95,69 @@ function executeaddon_json(params) {
         method: 'Addons.ExecuteAddon',
         params: {
             wait: false,
-            addonid: settings.input_addonid,
+            addonid: settings.get.input_addonid,
             params: params
         }
     };
     return JSON.stringify(jrpc);
 }
+
+
+settings.load();
+
+
+chrome.runtime.onConnect.addListener(function (port) {
+    console.assert(port.name == 'T2KASocket');
+    port.onMessage.addListener(function (msg) {
+        switch (msg.action) {
+            case 'save_settings':
+                if (msg.settings) {
+                    settings.save(msg.settings);
+                }
+                else {
+                  console.log('T2KASocket |save_settings| missing |settings|');
+                }                
+                break;        
+            case 'execute_addon':
+                if (msg.params) {
+                    settings.load(function () {
+                        execute_rpc(msg.action, msg.params)
+                    });
+                }
+                else {
+                  console.log('T2KASocket |execute_addon| missing |params|');
+                }
+                break;
+            case 'with_settings':
+                if (msg.cb_functions) {
+                    settings.load(function() {
+                        port.postMessage({ 
+                          action: 'with_settings', 
+                          settings: settings.get,
+                          cb_functions: msg.cb_functions
+                        });                        
+                    });
+                }
+                else {
+                  console.log('T2KASocket |with_settings| missing |cb_functions|');
+                }
+                break;            
+            default:
+                console.log('T2KASocket |No valid action provided|');
+        }
+    });
+});
+
+
+chrome.webNavigation.onHistoryStateUpdated.addListener(function (details) {
+    if (details.url.indexOf('://trakt.tv/') > -1) {
+        if ((settings.get.input_ip) && (settings.get.input_port) && (settings.get.input_addonid)) {
+            chrome.tabs.executeScript(details.tabId, {
+              file: '/js/content_script.js'
+            });
+            chrome.tabs.insertCSS(details.tabId, {
+              file: '/css/trakt.css'
+            });
+        }
+    }
+});
