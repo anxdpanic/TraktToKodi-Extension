1144cbb0ff601b1c8ebc7eb4ff888f3af64fc8ee
Tim Andrews <anxdpanic@users.noreply.github.com>
2016-04-30 19:43:07 -0400

    [trakt2kodiaddon] merge updates from future

--- js/background.js
+++ js/background.js
@@ -1,124 +1,151 @@
 var settings = {
-    _storage: function (action, arg1, arg2) {
-        var has_sync = (chrome.storage.sync !== undefined) ? true : false;
-        if (has_sync) {
-            switch (action) {
-                case 'set':
-                    chrome.storage.sync.set(arg1, arg2);
-                    break;
-                case 'get':
-                    chrome.storage.sync.get(arg1, arg2);
-                    break;
-                default:
-                    break;
-            }
-        }
-        else {
-            switch (action) {
-                case 'set':
-                    chrome.storage.local.set(arg1, arg2);
-                    break;
-                case 'get':
-                    chrome.storage.local.get(arg1, arg2);
-                    break;
-                default:
-                    break;
-            } 
-        }
-    },
-    defaults: {
-            input_ip: '',
-            input_port: '9090',
-            input_addonid: '',
-            input_movie_show_play: false,
-            input_episode_show_play: false,
-            input_episode_open_season: false,
-            input_output_format: '1'      
-    },
-    get: (this.defaults),
-    save: function (new_settings) {
-        settings._storage(
-            'set',
-            new_settings, 
-            function () {
-                settings.get = new_settings;
-            }
-        );
-    },
-    load: function (callback) {
-        settings._storage(
-            'get',
-            settings.defaults, 
-            function (items) {
-                settings.get = items;
-                if (callback) {
-                    callback();
-                }
-        });
-    }
+	_storage: function (action, arg1, arg2) {
+		var has_sync = (chrome.storage.sync !== undefined) ? true : false;
+		if (has_sync) {
+			switch (action) {
+				case 'set':
+					chrome.storage.sync.set(arg1, arg2);
+					break;
+				case 'get':
+					chrome.storage.sync.get(arg1, arg2);
+					break;
+				default:
+					break;
+			}
+		}
+		else {
+			switch (action) {
+				case 'set':
+					chrome.storage.local.set(arg1, arg2);
+					break;
+				case 'get':
+					chrome.storage.local.get(arg1, arg2);
+					break;
+				default:
+					break;
+			}
+		}
+	},
+	defaults: {
+		profiles: {
+			'active': '1',
+			'1': {
+				iphost: '',
+				port: '9090',
+				addonid: '',
+				format: '1'
+			},
+			'2': {
+				iphost: '',
+				port: '9090',
+				addonid: '',
+				format: '1'
+			},
+			'3': {
+				iphost: '',
+				port: '9090',
+				addonid: '',
+				format: '1'
+			},
+			'4': {
+				iphost: '',
+				port: '9090',
+				addonid: '',
+				format: '1'
+			}
+		},
+		movie_show_play: false,
+		episode_show_play: false,
+		episode_open_season: false,
+		sidebar_pagination: false
+	},
+	get: (this.defaults),
+	save: function (new_settings) {
+		settings._storage(
+			'set',
+			new_settings,
+			function () {
+				settings.get = new_settings;
+			}
+		);
+	},
+	load: function (callback) {
+		settings._storage(
+			'get',
+			settings.defaults,
+			function (items) {
+				settings.get = items;
+				if (callback) {
+					callback();
+				}
+		});
+	}
 };
 
 
 var rpc = {
-    can_connect: function () {
-        if ((settings.get.input_ip !== '') && (settings.get.input_port) && (settings.get.input_addonid !== '')) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    },
-    connection: function () {
-        this.url = 'ws://' + settings.get.input_ip + ':' + settings.get.input_port + '/jsonrpc';
-        this.socket = new WebSocket(this.url);
-    },
-    execute: function (action, params){
-        if (rpc.can_connect() !== true) {
-            console.log('rpc.execute(): Connection information missing/incomplete');
-            return;
-        }
-        var conn = new rpc.connection();
-        var log_lead = 'rpc.execute(\'' + action + '\'):\r\n|url| ' + conn.url + '\r\n';
-        switch (action) {
-            case 'execute_addon':
-                if (params) {
-                    var rpc_request = rpc.stringify.execute_addon(params);
-                    conn.socket.onopen = function (event) {
-                        console.log(log_lead + '|request| ' + rpc_request);
-                        conn.socket.send(rpc_request);
-                    };
-                    conn.socket.onmessage = function (event) {
-                        console.log(log_lead + '|response| ' + event.data);
-                        conn.socket.close();
-                    };
-                }
-                else {
-                    console.log('rpc.execute(\'' + action + '\'): missing |params|');
-                }
-                break;
-            default:
-                console.log('rpc.execute(): No |action| provided');
-                break;
-        }        
-    },
-    json: {
-        execute_addon: function (params) {
-            return  {   jsonrpc: '2.0',
-                        id: 1,
-                        method: 'Addons.ExecuteAddon',
-                        params: {
-                            wait: false,
-                            addonid: settings.get.input_addonid,
-                            params: params
-                        }
-                    }
-        }
-    },
-    stringify: {
-        execute_addon: function (params) {
-            return JSON.stringify(rpc.json.execute_addon(params));
-        }
-    }
+	can_connect: function () {
+		var active = settings.get.profiles.active;
+		if ((settings.get.profiles[active].iphost !== '') && (settings.get.profiles[active].port) && (settings.get.profiles[active].addonid !== '')) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	},
+	connection: function () {
+		var active = settings.get.profiles.active;
+		this.url = 'ws://' + settings.get.profiles[active].iphost + ':' + settings.get.profiles[active].port + '/jsonrpc';
+		this.socket = new WebSocket(this.url);
+	},
+	execute: function (action, params){
+		if (rpc.can_connect() !== true) {
+			console.log('rpc.execute(): Connection information missing/incomplete');
+			return;
+		}
+		var conn = new rpc.connection();
+		var log_lead = 'rpc.execute(\'' + action + '\'):\r\n|url| ' + conn.url + '\r\n';
+		switch (action) {
+			case 'execute_addon':
+				if (params) {
+					var rpc_request = rpc.stringify.execute_addon(params);
+					conn.socket.onopen = function (event) {
+						console.log(log_lead + '|request| ' + rpc_request);
+						conn.socket.send(rpc_request);
+					};
+					conn.socket.onmessage = function (event) {
+						console.log(log_lead + '|response| ' + event.data);
+						conn.socket.close();
+					};
+				}
+				else {
+					console.log('rpc.execute(\'' + action + '\'): missing |params|');
+				}
+				break;
+			default:
+				console.log('rpc.execute(): No |action| provided');
+				break;
+		}
+	},
+	json: {
+		execute_addon: function (params) {
+			var active = settings.get.profiles.active;
+			return {	jsonrpc: '2.0',
+						id: 1,
+						method: 'Addons.ExecuteAddon',
+						params: {
+							wait: false,
+							addonid: settings.get.profiles[active].addonid,
+							params: params
+						}
+					}
+		}
+	},
+	stringify: {
+		execute_addon: function (params) {
+			return JSON.stringify(rpc.json.execute_addon(params));
+		}
+	}
 }
 
 
@@ -126,58 +153,68 @@ settings.load();
 
 
 chrome.runtime.onConnect.addListener(function (port) {
-    console.assert(port.name == 'T2KASocket');
-    port.onMessage.addListener(function (msg) {
-        switch (msg.action) {
-            case 'save_settings':
-                if (msg.settings) {
-                    settings.save(msg.settings);
-                }
-                else {
-                  console.log('T2KASocket: |save_settings| missing |settings|');
-                }                
-                break;        
-            case 'execute_addon':
-                if (msg.params) {
-                    settings.load(function () {
-                        rpc.execute(msg.action, msg.params);
-                    });
-                }
-                else {
-                  console.log('T2KASocket: |execute_addon| missing |params|');
-                }
-                break;
-            case 'with_settings':
-                if (msg.cb_functions) {
-                    settings.load(function() {
-                        port.postMessage({ 
-                          action: 'with_settings', 
-                          settings: settings.get,
-                          cb_functions: msg.cb_functions
-                        });                        
-                    });
-                }
-                else {
-                  console.log('T2KASocket: |with_settings| missing |cb_functions|');
-                }
-                break;            
-            default:
-                console.log('T2KASocket: No valid |action| provided');
-                break;
-        }
-    });
+	console.assert(port.name == 'T2KASocket');
+	port.onMessage.addListener(function (msg) {
+		switch (msg.action) {
+			case 'save_settings':
+				if (msg.settings) {
+					settings.save(msg.settings);
+				}
+				else {
+					console.log('T2KASocket: |save_settings| missing |settings|');
+				}
+				break;
+			case 'execute_addon':
+				if (msg.params) {
+					settings.load(function () {
+						rpc.execute(msg.action, msg.params);
+					});
+				}
+				else {
+					console.log('T2KASocket: |execute_addon| missing |params|');
+				}
+				break;
+			case 'with_settings':
+				if (msg.cb_functions) {
+					settings.load(function() {
+						port.postMessage({
+							action: 'with_settings',
+							settings: settings.get,
+							cb_functions: msg.cb_functions
+						});
+					});
+				}
+				else {
+					console.log('T2KASocket: |with_settings| missing |cb_functions|');
+				}
+				break;
+			default:
+				console.log('T2KASocket: No valid |action| provided');
+				break;
+		}
+	});
+});
+
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	switch(request.action) {
+		case 'active_format':
+			sendResponse({active_format: settings.get.profiles[settings.get.profiles.active].format});
+			break;
+	}
+	return true;
 });
 
 
 chrome.webNavigation.onHistoryStateUpdated.addListener(function (details) {
-    if (details.url.indexOf('://trakt.tv/') > -1) {
-        if (rpc.can_connect() === true) {
-            chrome.tabs.executeScript(details.tabId, {
-              file: '/js/content_script.js'
-            });
-            chrome.tabs.insertCSS(details.tabId, {
-              file: '/css/trakt.css'
-            });
-        }
-    }
+	if (details.url.indexOf('://trakt.tv/') > -1) {
+		if (rpc.can_connect() === true) {
+			chrome.tabs.executeScript(details.tabId, {
+				file: '/js/content_script.js'
+			});
+			chrome.tabs.insertCSS(details.tabId, {
+				file: '/css/trakt.css'
+			});
+		}
+	}
 });
--- js/settings.js
+++ js/settings.js
@@ -2,123 +2,146 @@ var port = chrome.runtime.connect({ name: 'T2KASocket' });
 
 
 var settings = {
-    get: (null),
-    load: function (callback_array) {
-        port.postMessage({ 
-          action: 'with_settings', 
-          cb_functions: callback_array
-        });      
-    },
-    update: function () {
-        document.getElementById('input-ip').value = settings.get.input_ip;
-        document.getElementById('input-port').value = settings.get.input_port;
-        document.getElementById('input-addonid').value = settings.get.input_addonid;
-        document.getElementById('input-movie-show-play').checked = settings.get.input_movie_show_play;
-        document.getElementById('input-episode-show-play').checked = settings.get.input_episode_show_play;
-        document.getElementById('input-episode-open-season').checked = settings.get.input_episode_open_season;        
-        var rads = document.getElementsByClassName('settings radio');
-        var _length = rads.length;
-        for (var i = 0; i < _length; i++) {
-            if (rads[i].value === settings.get.input_output_format) {
-                rads[i].checked = true;
-                break;
-            }
-        }    
-    },
-    save: function () {
-        var rads = document.getElementsByClassName('settings radio');
-        var input_output_format = '1';
-        var _length = rads.length;
-        for (var i = 0; i < _length; i++) {
-            if (rads[i].checked) {
-                input_output_format = rads[i].value;
-                break;
-            }
-        }
-        var new_settings = {
-            input_ip: document.getElementById('input-ip').value,
-            input_port: document.getElementById('input-port').value,
-            input_addonid: document.getElementById('input-addonid').value,
-            input_movie_show_play: document.getElementById('input-movie-show-play').checked,
-            input_episode_show_play: document.getElementById('input-episode-show-play').checked,
-            input_episode_open_season: document.getElementById('input-episode-open-season').checked,
-            input_output_format: input_output_format        
-        }
-        port.postMessage({ action: 'save_settings', settings: new_settings }); 
-    },
-    clear: function () {
-        var elements = document.getElementsByClassName('settings text');
-        var _length = elements.length;
-        for (var i = 0; i < _length; i++) {
-            if (elements[i].id == 'input-port') {
-                elements[i].value = '9090';
-            }
-            else {
-                elements[i].value = '';
-            }
-        }
-    }
+	get: (null),
+	load: function (callback_array) {
+		port.postMessage({
+			action: 'with_settings',
+			cb_functions: callback_array
+		});
+	},
+	update: function () {
+		var active_profile = settings.get.profiles.active;
+		var s = '';
+		var j = 0;
+		var i = 0;
+		var rads = document.querySelectorAll('input[name="tabs_sub1"]');
+		var _length = rads.length;
+		for (i = 0; i < _length; i++) {
+			if (rads[i].value === active_profile) {
+				rads[i].checked = true;
+				break;
+			}
+		}
+		for (i = 1; i < 5; i++) {
+			s = i.toString();
+			document.querySelector('input[id="iphost_' + s + '"]').value = settings.get.profiles[s].iphost;
+			document.querySelector('input[id="port_' + s + '"]').value = settings.get.profiles[s].port;
+			document.querySelector('input[id="addonid_' + s + '"]').value = settings.get.profiles[s].addonid;
+			rads = document.querySelectorAll('input[name="formats_' + s + '"]');
+			_length = rads.length;
+			for (j = 0; j < _length; j++) {
+				if (rads[j].value === settings.get.profiles[s].format) {
+					rads[j].checked = true;
+					break;
+				}
+			}
+		}
+		document.querySelector('#movie-show-play').checked = settings.get.movie_show_play;
+		document.querySelector('#episode-show-play').checked = settings.get.episode_show_play;
+		document.querySelector('#episode-open-season').checked = settings.get.episode_open_season;
+		document.querySelector('#sidebar-pagination').checked = settings.get.sidebar_pagination;
+	},
+	save: function () {
+		var new_settings = {
+			profiles: {
+				'active': document.querySelector('input[name="tabs_sub1"]:checked').value,
+				'1': {
+					iphost: document.querySelector('input[id="iphost_1"]').value,
+					port: document.querySelector('input[id="port_1"]').value,
+					addonid: document.querySelector('input[id="addonid_1"]').value,
+					format: document.querySelector('input[name="formats_1"]:checked').value
+				},
+				'2': {
+					iphost: document.querySelector('input[id="iphost_2"]').value,
+					port: document.querySelector('input[id="port_2"]').value,
+					addonid: document.querySelector('input[id="addonid_2"]').value,
+					format: document.querySelector('input[name="formats_2"]:checked').value
+				},
+				'3': {
+					iphost: document.querySelector('input[id="iphost_3"]').value,
+					port: document.querySelector('input[id="port_3"]').value,
+					addonid: document.querySelector('input[id="addonid_3"]').value,
+					format: document.querySelector('input[name="formats_3"]:checked').value
+				},
+				'4': {
+					iphost: document.querySelector('input[id="iphost_4"]').value,
+					port: document.querySelector('input[id="port_4"]').value,
+					addonid: document.querySelector('input[id="addonid_4"]').value,
+					format: document.querySelector('input[name="formats_4"]:checked').value
+				}
+			},
+			movie_show_play: document.querySelector('#movie-show-play').checked,
+			episode_show_play: document.querySelector('#episode-show-play').checked,
+			episode_open_season: document.querySelector('#episode-open-season').checked,
+			sidebar_pagination: document.querySelector('#sidebar-pagination').checked
+		}
+		port.postMessage({ action: 'save_settings', settings: new_settings });
+	}
 }
 
 
 function load_version () {
-    var version_element = document.getElementById('extension_version');
-    var inner_text = document.createTextNode(chrome.i18n.getMessage('version') + ': ' + chrome.runtime.getManifest().version);
-    version_element.appendChild(inner_text);
+	var version_element = document.getElementById('extension_version');
+	var inner_text = document.createTextNode(chrome.i18n.getMessage('version') + ': ' + chrome.runtime.getManifest().version);
+	version_element.appendChild(inner_text);
 }
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    load_version();
-    settings.load(['settings.update']);
-});
-
-
-document.getElementById('button-clear').addEventListener('click', function () {
-    settings.clear();
+	load_version();
+	settings.load(['settings.update']);
 });
 
 
 document.getElementById('button-save').addEventListener('click', function () {
-    settings.save();
+	settings.save();
 });
 
 
 var checkboxes = document.getElementsByClassName('settings checkbox');
 var _length = checkboxes.length;
 for (var i = 0; i < _length; i++) {
-    checkboxes[i].addEventListener('click', function () {
-        settings.save();
-    });
+	checkboxes[i].addEventListener('click', function () {
+		settings.save();
+	});
 }
 
 
 var radios = document.getElementsByClassName('settings radio');
 _length = radios.length;
 for (i = 0; i < _length; i++) {
-    radios[i].addEventListener('click', function () {
-        settings.save();
-    });    
+	radios[i].addEventListener('click', function () {
+		settings.save();
+	});
+}
+
+
+radios = document.querySelectorAll('input[name="tabs_sub1"]');
+_length = radios.length;
+for (i = 0; i < _length; i++) {
+	radios[i].addEventListener('click', function () {
+		settings.save();
+	});
 }
 
 
 port.onMessage.addListener(function(msg) {
-  switch (msg.action) {
-    case 'with_settings':
-      if ((msg.cb_functions) && (msg.settings)) {
-        settings.get = msg.settings;
-        for (var i = 0; i < msg.cb_functions.length; i++) {
-            switch(msg.cb_functions[i]) {
-                case 'settings.update':
-                    settings.update();
-                    break;
-                default:
-                    break;
-            }
-        }
-      }
-      break;
-    default:
-      break;
-  }
+	switch (msg.action) {
+		case 'with_settings':
+			if ((msg.cb_functions) && (msg.settings)) {
+				settings.get = msg.settings;
+				for (var i = 0; i < msg.cb_functions.length; i++) {
+					switch(msg.cb_functions[i]) {
+						case 'settings.update':
+							settings.update();
+							break;
+						default:
+							break;
+					}
+				}
+			}
+			break;
+		default:
+			break;
+	}
 });
